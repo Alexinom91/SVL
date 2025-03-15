c1ffea4d5d6e282fc9f4bb6136bc6f93639f00ce
Alexinom91 <109924047+Alexinom91@users.noreply.github.com>
2025-03-15 16:38:02 +0100

    first working version

--- test/test.c
+++ test/test.c
@@ -1,10 +1,13 @@
 #include "../include/svl.h"
 
+
 int main()
 {
     svl vector;
-    value val;
-    val.i = 10;
     svl_new(vector, INT);
-    svl_assign(vector, 0, val, INT);
+    svl_assign(vector, 0, 20, INT);
+    printf("%d\n", vector->data[0].i);
+    getchar();
+
+    return 0;
 }
